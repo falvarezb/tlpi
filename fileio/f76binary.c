b541ff778f556b07b5121589f718846a5b5c0f16
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2021-03-05 22:41:56 +0000

    f76binary

---
+++ fileio/f76binary.c
@@ -0,0 +1,87 @@
+/**
+ * Copy the hexadecimal representation of the file argv[1] into
+ * the file argv[2] (or stdout if there is no argv[2])
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUF_SIZE 1024
+#define BUF_EXPANSION_FACTOR 3
+
+/*
+    'num_copy' elements of orig_buffer are converted into hexadecimal and copied
+    to dest_buffer
+*/
+void hex(unsigned char *dest_buffer, unsigned char *orig_buffer, int num_copy) {
+    char buf_temp[BUF_EXPANSION_FACTOR + 1];
+    for (size_t i = 0; i < num_copy; i++) {
+        if (sprintf(buf_temp, "%.2x ", orig_buffer[i]) < 0) {
+            printf("error %d while formatting output %c", errno,
+                   orig_buffer[i]);
+        }
+        dest_buffer[i * 3] = buf_temp[0];      // first hexadecimal digit
+        dest_buffer[i * 3 + 1] = buf_temp[1];  // second hexadecimal digit
+        dest_buffer[i * 3 + 2] = buf_temp[2];  // whitespace
+    }
+}
+
+int main(int argc, char *argv[]) {
+    FILE *inputFd, *outputFd;
+    size_t numRead;
+    // 0 <= read_buf[i] <= 255
+    unsigned char read_buf[BUF_SIZE];
+    // each read_buf[i] is expanded into its hexadecimal representation +
+    // whitespace
+    unsigned char write_buf[BUF_SIZE * BUF_EXPANSION_FACTOR];
+
+    if ((argc != 2 && argc != 3 ) || strcmp(argv[1], "--help") == 0) {
+        printf("%s old-file [new-file]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Open input and output files */
+
+    inputFd = fopen(argv[1], "rb");
+    if (inputFd == NULL) {
+        printf("error %d while opening file %s", errno, argv[1]);
+        perror("");
+        exit(EXIT_FAILURE);
+    }
+
+    if (argc == 2) 
+        outputFd = stdout;    
+    else
+        outputFd = fopen(argv[2], "w");    
+    
+    if (outputFd == NULL) {
+        printf("error %d while opening file %s", errno, argv[2]);
+        perror("");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Transfer data until we encounter end of input or an error */
+    while ((numRead = fread(read_buf, 1, BUF_SIZE, inputFd)) > 0) {
+        if (numRead < BUF_SIZE && ferror(inputFd)) {
+            perror("read");
+            printf("error %d while reading file %s", errno, argv[1]);
+            exit(EXIT_FAILURE);
+        }
+
+        hex(write_buf, read_buf, numRead);
+
+        if (fwrite(write_buf, 1, numRead * BUF_EXPANSION_FACTOR, outputFd) != numRead * BUF_EXPANSION_FACTOR) {
+            perror("write() returned error or partial write occurred");
+            printf("error %d while writing file %s", errno, argv[1]);
+            exit(EXIT_FAILURE);
+        }
+    }
+    if (ferror(inputFd)) perror("read");
+
+    if (fclose(inputFd) == EOF) perror("close input");
+    if (fclose(outputFd) == EOF) perror("close output");
+
+    exit(EXIT_SUCCESS);
+}
