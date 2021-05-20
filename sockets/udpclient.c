1f325210d79edec3454c71c21e80a1ea7864097d
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2021-05-20 21:37:43 +0100

    udp refactoring

--- sockets/udpclient.c
+++ sockets/udpclient.c
@@ -1,99 +1,38 @@
 #include <netdb.h>
-#include <errno.h>
+#include <netinet/in.h>
 #include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <sys/socket.h>
+#include "tlpi_hdr.h"
+#include "inet_sockets.h"
 
-#define BUF_SIZE 1024*10
+#define BUF_SIZE 1024
 #define PORT_NUM "6666"
 
-void errorExit(char *format, const char *text) {
-    printf(format, errno, text);
-    perror("");
-    exit(EXIT_FAILURE);
-}
-
-int open_file(const char *filename) {
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1) {
-        errorExit("error %d while opening file %s", filename);
-    }
-    return fd;
-}
-
-int establish_connection(const char *hostname) {
-     // ----------- 1. Create socket object ------------------
-
-    int conn_sd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (conn_sd == -1) {
-        fprintf(stderr, "Could not create socket: %s\n",
-                strerror(errno));
-        exit(1);
-    }
-
-    // ------------ 2. Connect to server-- ------------------
-
-    // Find the IP address behind the hostname
-    struct hostent* host_entry;
-    host_entry = gethostbyname(hostname);
-    if (!host_entry) {
-        fprintf(stderr, "Could not resolve the host '%s'\n", hostname);
-        exit(1);
-    }
-
-    // Prepare the address
-    struct sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_addr = *((struct in_addr*)host_entry->h_addr);;
-    addr.sin_port = htons(6666);
-
-    int result = connect(conn_sd, (struct sockaddr*)&addr,
-            sizeof(addr));
-    if (result == -1) {
-        close(conn_sd);
-        fprintf(stderr, "Could no connect: %s\n", strerror(errno));
-        exit(1);
-    }
-    return conn_sd;
-}
-
-int
-main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     ssize_t num_read;
-    ssize_t tot_sent = 0;
-    char buf[BUF_SIZE];
+    ssize_t tot_sent = 0;    
+    char buf[BUF_SIZE];   
+    int sfd, fd; 
 
-    if (argc < 4 || strcmp(argv[1], "--help") == 0) {
-        printf("%s server-host file num-requests \n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+    if (argc < 4 || strcmp(argv[1], "--help") == 0) usageErr("%s server-host file num-requests\n", argv[0]);
+    char *serverhost = argv[1];
+    char *filename = argv[2];
+    size_t num_req = atoi(argv[3]);
 
-    int sfd = establish_connection(argv[1]);
-    //Multiple requests on same connection
-    int num_req = atoi(argv[3]);
-    for (size_t i = 0; i < num_req; i++) {
-        
-        int fd = open_file(argv[2]);
+    if ((sfd = inetConnect(argv[1], PORT_NUM, SOCK_DGRAM)) == -1) errExit("error while connecting to %s", serverhost);    
+
+    //Multiple requests on same connection    
+    for (size_t i = 0; i < num_req; i++) {        
+        if((fd = open(filename, O_RDONLY)) == -1) errExit("error while opening file %s\n", filename);
 
         while ((num_read = read(fd, buf, BUF_SIZE)) > 0) {
-            if (write(sfd, buf, num_read) != num_read)
-                errorExit("error %d while writing to socket %s\n", "");
-
+            if (write(sfd, buf, num_read) != num_read) errExit("error while sending data");                    
+            if (num_read == -1) errExit("error %d while reading file %s\n", filename);
             tot_sent += num_read;
-            //printf("Total sent: %zd\n", tot_sent);
         }
-
-        if (num_read == -1)
-            errorExit("error %d while reading file %s\n", argv[2]);
-        if (close(fd) == -1) perror("close file");   
-          
+        
+        if (close(fd) == -1) errMsg("close input");        
     }
-    if (close(sfd) == -1) perror("close socket"); 
     printf("Total sent: %zd\n", tot_sent);
 
     exit(EXIT_SUCCESS); /* Closes 'sfd' */
--- sockets/udpserver.c
+++ sockets/udpserver.c
@@ -1,88 +1,41 @@
-#define _BSD_SOURCE             /* To get definitions of NI_MAXHOST and
-                                   NI_MAXSERV from <netdb.h> */
 #include <netdb.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <signal.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include "tlpi_hdr.h"
+#include "inet_sockets.h"
 
-
-#define BACKLOG 50
 #define BUF_SIZE 1024
 #define PORT_NUM "6666"
-
-void errorExit(char *format, const char *text) {
-    printf(format, errno, text);
-    perror("");
-    exit(EXIT_FAILURE);
-}
-
-int open_listener_socket() {
-    // ----------- 1. Create socket object ------------------
-    int server_sd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (server_sd == -1) {
-        fprintf(stderr, "Could not create socket: %s\n",
-                strerror(errno));
-        exit(1);
-    }
-
-    // ----------- 2. Bind the socket file ------------------
-
-    // Prepare the address
-    struct sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(6666);
-
-    int result = bind(server_sd,
-            (struct sockaddr*)&addr, sizeof(addr));
-    if (result == -1) {
-        close(server_sd);
-        fprintf(stderr, "Could not bind the address: %s\n",
-                strerror(errno));
-        exit(1);
-    }
-
-    return server_sd;
-}
 
 int
 main(int argc, char *argv[])
 {
-    ssize_t num_read;
-    ssize_t tot_received = 0;
-    struct sockaddr_storage claddr;
-    int lfd, cfd, reqLen;
-    socklen_t addrlen;
-#define ADDRSTRLEN (NI_MAXHOST + NI_MAXSERV + 10)
-    char addrStr[ADDRSTRLEN];
-    char host[NI_MAXHOST];
-    char service[NI_MAXSERV];
+    int sfd;
+    ssize_t num_read, tot_received;                
     char buf[BUF_SIZE];
-
 
     /* Ignore the SIGPIPE signal, so that we find out about broken connection
        errors via a failure from write(). */
 
-    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)    perror("signal");
+    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) errExit("signal");
 
-    cfd = open_listener_socket();
-    printf("listening for connections on port %s...\n", PORT_NUM);    
-                
-    //sleep(10);
-    while ((num_read = read(cfd, buf, BUF_SIZE)) > 0) {
-        tot_received += num_read;
-        printf("Total received: %zd\n", tot_received); 
-    }
-
-    if (num_read == -1) errorExit("error %d while reading socket %s\n", "");
+    sfd = inetBind(PORT_NUM, SOCK_DGRAM, NULL);
+    if (sfd == -1) errExit("error while opening socket");
     
-    close(cfd);
-    printf("end\n");
+    printf("listening for connections on port %s...\n", PORT_NUM);
 
+    while(1) {                  /* Handle clients iteratively */
+
+        tot_received = 0;        
+        
+        while ((num_read = read(sfd, buf, BUF_SIZE)) > 0) {
+            tot_received += num_read;
+            printf("Total received: %zd\n", tot_received);
+        }
+
+        if (num_read == -1) errExit("error while reading client data"); 
+
+        if (close(sfd) == -1)           /* Close connection */
+            errMsg("close connection");
+    }
 }
